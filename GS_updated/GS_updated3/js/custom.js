dc8dc1a1f47d18eba49d67ff9acc50011aa10647
free energy <asborsody@gmail.com>
2012-01-22 10:48:50 -0500

    alex changes, tooltip, mockup effect of answers

--- GS_updated/GS_updated3/js/custom.js
+++ GS_updated/GS_updated3/js/custom.js
@@ -68,7 +68,12 @@
        
        /* Form Validations */ /* Send-Receive Tab Form   */
        
+       
+       
+       
         $("#send-receive-submit").click(function(){
+           
+        
         if($("ul#sr-media-type-ul input[name=radMediaType]").is(':checked')){
             if($("ul#sr-media-type-ul input[name=radMediaType]").val()=='electronic'){
          sr_destination=$("#send-receive-destination input[name=dest]").is(':checked')
@@ -83,14 +88,54 @@
             $("#sr-recipient-block").removeClass('green-text');
             $("#sr-recipient-block").addClass('red-text');
          }
-         if(sr_destination&&sr_recipient){
+
+		 sr_frequency=$("#send-receive-frequency input[name=radFrequency]").is(':checked')
+         if(!sr_recipient){
+            $("#sr-frequency-block").removeClass('green-text');
+            $("#sr-frequency-block").addClass('red-text');
+         }
+		 sr_datatype=$("#send-receive-datatype input[name=radDataType]").is(':checked')
+         if(!sr_recipient){
+            $("#sr-datatype-block").removeClass('green-text');
+            $("#sr-datatype-block").addClass('red-text');
+         }
+
+
+
+
+         if(sr_destination&&sr_recipient&&sr_frequency&&sr_datatype){
+                  $("#sr-datatype-block").addClass('green-text');
+            $("#sr-datatype-block").removeClass('red-text');
+
+			 $("#sr-frequency-block").addClass('green-text');
+            $("#sr-frequency-block").removeClass('red-text');
+
+
                 $("#sr-dest-block").removeClass('red-text');
                 $("#sr-recipient-block").removeClass('red-text');
                  $("#sr-recipient-block").addClass('green-text');
                   $("#sr-recipient-block").addClass('green-text');
                   $("#sr-mediatype").removeClass('red-text');
                  $("#sr-mediatype").addClass('green-text');
-                 $(this).val('Update Requirements')
+                 $(this).val('Update Requirements');
+		 
+		 //answer demonstration: fade in mock answer if questions selected
+	 $(".button-block").click(function(){
+	    $("#answer").fadeIn('slow');
+	    
+	     $("#mediaType2").click( function(){
+		$("#answer").hide();
+		
+	     });
+	 });
+   
+    
+    
+
+		 
+		 
+		 
+		 
          }
             }
             else{
@@ -169,12 +214,7 @@
 
 
 /*-------------------------------------------------------------------- 
- * jQuery plugin: customInput()
- * by Maggie Wachs and Scott Jehl, http://www.filamentgroup.com
- * Copyright (c) 2009 Filament Group
- * Dual licensed under the MIT (filamentgroup.com/examples/mit-license.txt) and GPL (filamentgroup.com/examples/gpl-license.txt) licenses.
- * Article: http://www.filamentgroup.com/lab/accessible_custom_designed_checkbox_radio_button_inputs_styled_css_jquery/  
- * Usage example below (see comment "Run the script...").
+customInput()
 --------------------------------------------------------------------*/
 
 
@@ -236,4 +276,50 @@ jQuery.fn.customInput = function(){
 
 	
 	
+ 
+$(document).ready(function() {
+    
+    
+        
+    
+ 
+    //Select all anchor tag with rel set to tooltip
+    $('a[rel=tooltip]').mouseover(function(e) {
+         
+        //Grab the title attribute's value and assign it to a variable
+        var tip = $(this).attr('title');   
+         
+        //Remove the title attribute's to avoid the native tooltip from the browser
+        $(this).attr('title','');
+         
+        //Append the tooltip template and its value
+        $(this).append('<div id="tooltip"><div class="tipHeader"></div><div class="tipBody">' + tip + '</div><div class="tipFooter"></div></div>');    
+         
+        //Set the X and Y axis of the tooltip
+        $('#tooltip').css('top', e.pageY + 5 );
+        $('#tooltip').css('left', e.pageX + 5 );
+         
+        //Show the tooltip with fadeIn effect
+        $('#tooltip').fadeIn('slow');
+        $('#tooltip').fadeTo('10',0.8);
+         
+    }).mousemove(function(e) {
+     
+        //Keep changing the X and Y axis for the tooltip, thus, the tooltip move along with the mouse
+        $('#tooltip').css('top', e.pageY + 5 );
+        $('#tooltip').css('left', e.pageX + 5 );
+         
+    }).mouseout(function() {
+     
+        //Put back the title attribute's value
+        $(this).attr('title',$('.tipBody').html());
+     
+        //Remove the appended tooltip template
+        $(this).children('div#tooltip').remove();
+         
+    });
+ 
+});
+ 
+
 
